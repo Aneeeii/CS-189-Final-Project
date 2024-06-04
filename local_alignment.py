0bd69c3cf2a181015f619371778f7a6e6c3dcf96
Annie Yin <anniesy2@uci.edu>
2024-06-04 01:35:27 -0700

    fill_chart begins filling the chart in after initialization

--- local_alignment.py
+++ local_alignment.py
@@ -63,14 +63,56 @@ def create_matrix(seqa: list, seqb: list, match):
     return align_chart
 
 
-def fill_chart(chart, gap, mismatch, match, start_pos):
+def fill_chart(chart, gap, mismatch, match, start_pos, seqa, seqb):
     down_fill = start_pos[0]
     across_fill = start_pos[1]
+
+    for i in range(down_fill, len(seqa)):
+        previous = chart[i - 1][across_fill - 1]
+        previous_value = previous.value()
+        left = chart[i][across_fill - 1]
+        up = chart[i - 1][across_fill]
+        left_value = left.value()
+        up_value = up.value()
+        leads = []
+        value = 0
+        if seqa[i] == seqb[across_fill]:  # match
+            value = previous_value + match
+            leads.append(previous.position())
+        if value <= previous_value + mismatch: # mismatch
+            if value == previous_value + mismatch and previous.position() not in leads:
+                leads.append(previous.position())
+            else:
+                value = previous_value + mismatch
+                leads = [previous.position()]
+        if value <= up_value + gap:  # gap from top
+            if value == up_value + gap and up.position() not in leads:
+                leads.append(up.position())
+            else:
+                value = up_value + gap
+                leads = [up.position()]
+        if value <= left_value + gap:  # gap from side
+            if value == left_value + gap and left.position() not in leads:
+                leads.append(left.position())
+            else:
+                value = left_value + gap
+                leads = [left.position()]
+            
+        if value <= 0:
+            unit = ZeroMatrixUnit((i, across_fill))
+        else:
+            unit = MatrixUnit(value, (i, across_fill))
+            for k in leads:
+                unit.add_leading(k)
+        
+        chart[i].append(unit)
     
+    return chart
 
 
 a = create_matrix(["a", "b", "c"], ["b", "b", "c", "a"], 2)
-for i in a:
-    print(i)
 
-fill_chart(a, -2, -1, 2, (1, 1))
+b = fill_chart(a, -2, -1, 2, (1, 1), ["a", "b", "c"], ["b", "b", "c", "a"])
+
+for i in b:
+    print(i)
