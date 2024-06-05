7d3ad42ad0eac3b9ef3ca05476c3566f7a23d390
Ryan Kin-Hin Kwok <kwokrk@uci.edu>
2024-06-05 01:06:17 -0700

    Everything should work now

--- global_alignment.py
+++ global_alignment.py
@@ -12,27 +12,6 @@ def initialize_chart(seqa: list, seqb: list, gap):
         item.insert(0, i * gap)
     return values
 
-        
-        
-    # for i in first_row:
-    #     index = first_row.index(i)
-    #     first_row.insert(index, index * gap)
-    # print(first_row)
-
-    # for i in range(len(seqa)):
-    #     first_row.append([points, (0, i)])
-    #     points += gap
-    # align_chart.append(first_row)
-
-    # points = gap
-    # for i in range(1, len(seqb)):
-    #     align_chart.append([(points, (i, 0))])
-    #     for o
-    #     points += gap
-    
-    # return align_chart
-
-
 def correct_sequences(seqa, seqb, gap):
     length_difference = len(seqa) - len(seqb)
     adding, ref = seqa, seqb
@@ -43,16 +22,31 @@ def correct_sequences(seqa, seqb, gap):
         ref.append(adding[-1])
     return initialize_chart(seqa, seqb, gap)
 
-def fill_score(sequences, match_info, chart):
+def fill_score(sequences, match_info):
     top_seq, bot_seq = sequences
-    for x in range(1, len(top_seq)+1):
-        for y in range(1, len(top_seq)+1):
+    LT, LB = len(top_seq), len(bot_seq)
+    if LT > LB:
+        longer = LT
+    elif LB > LT:
+        longer = LB
+    else:
+        longer = LT
+    chart = correct_sequences(top_seq, bot_seq, match_info[2])
+    for x in range(1, longer+1):
+        for y in range(1, longer+1):
             diagonal_score = chart[x-1][y-1][0]
             top_score = chart[x][y-1][0]
             bot_score = chart[x-1][y][0]
             scores = (diagonal_score, top_score, bot_score)
             result = find_optimal_score(top_seq, bot_seq, (x,y), scores, match_info)
             chart[x][y].insert(0, result)
+    temporary_chart = chart[:]
+    if LT > LB:
+        chart = chart[:LB+1]
+    elif LB > LT:
+        for index, row in enumerate(temporary_chart):
+            row = row[:LT+1]
+            chart[index] = row
     return chart
 
 def find_optimal_score(top_seq, bot_seq, coordinates, scores, match_info):
@@ -68,13 +62,79 @@ def find_optimal_score(top_seq, bot_seq, coordinates, scores, match_info):
     return max(new_diagonal, new_bot, new_top)
 
 
-a = list("GTCGACGCA")
-b = list("GATTACAAA")
-y = initialize_chart(a, b, -2)
+def find_alignment(chart, sequences):
+    top_seq, bot_seq = sequences
+    last_row, last_column = len(chart)-1, len(chart[0])-1
+    top_aligned = []
+    bot_aligned = []
+    for i in chart:
+        print(i)
+    first = True
+    while True:
+        if first:
+            if top_seq[-1] == bot_seq[-1]:
+                top_aligned.append(top_seq[-1])
+                bot_aligned.append(top_seq[-1])
+            else:
+                top_aligned.append(top_seq[-1])
+                bot_aligned.append(bot_seq[-1])
+            first = False
+        check = chart[last_row][last_column]
+        if chart[last_row][last_column][1] == [1,1]: #(last_row, last_column) == (-1,-1)
+            # top_aligned.pop(-1)
+            # bot_aligned.pop(-1)
+            break
+        print(check)
+        # print(last_row-1, last_column-1)
+        print(top_seq[last_column-2], bot_seq[last_row-2])
+        # break
+        if top_seq[last_column-2] == bot_seq[last_row-2]:
+            last_row, last_column = last_row-1, last_column-1
+            to_add = top_seq[last_column-1]
+            top_aligned.append(to_add)
+            bot_aligned.append(to_add)
+            continue
+        diagonal = chart[last_row-1][last_column-1][0]
+        top = chart[last_row-1][last_column][0]
+        left = chart[last_row][last_column-1][0]
+        # print(diagonal, top, left)
+        values = [diagonal, top, left]
+        greatest = max(values)
+        if values.count(greatest) > 1:
+            if values[0] == greatest:
+                top_aligned.append(top_seq[last_column-2])
+                bot_aligned.append(bot_seq[last_row-2])
+                last_row, last_column = last_row-1, last_column-1
+        elif values[0] == greatest:
+            if last_row != 0 and last_column != 0:
+                top_aligned.append(top_seq[last_column-1]) # prob change to 2
+                bot_aligned.append(bot_seq[last_row-1]) # prob change to 2
+            last_row, last_column = last_row-1, last_column-1
+        elif values[1] == greatest:
+            top_aligned.append("-")
+            bot_aligned.append(bot_seq[last_row-2])
+            last_row, last_column = last_row-1, last_column
+        elif values[2] == greatest:
+            bot_aligned.append("-")
+            top_aligned.append(top_seq[last_column-2])
+            last_row, last_column = last_row, last_column-1
+        # x = input()
+        # print(diagonal, top, left)
+        # break
+    # print(chart[last_row][last_column])
+    print(list(reversed(top_aligned)), list(reversed(bot_aligned)))
+
+# y = initialize_chart(a, b, -2)
 # for i in y:
 #     print(i)
 
-
-z = global_alignment((a,b), (1,-1,-2), y)
-for i in z:
-    print(i)
+def main():
+    a = list("TCATG")
+    b = list("CATTG")
+    match_info = (1,-1,-2)
+    chart = fill_score((a,b), match_info)
+    find_alignment(chart, (a,b))
+# z = fill_score((a,b), (1,-1,-2))
+# for i in z:
+#     print(i)
+main()
